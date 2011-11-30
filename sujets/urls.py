6534ebda14bb5809be67cf185849940b84284b92
Georges Dubus <georges.dubus@compiletoi.net>
2011-11-30 13:33:44 +0100

    Added pages to create questions and options.

--- sujets/urls.py
+++ sujets/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from sujets.models import Question
-from sujets.views import vote
+from sujets.views import vote, OptionCreateView
 
 
 urlpatterns = patterns('',
 	url(r'^$', ListView.as_view(model=Question)),
 	url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Question), name='question_detail'),
-	url(r'^vote/$', vote, name="vote")
+    url(r'^new/$', CreateView.as_view(model=Question), name='question_new'),
+    url(r'^(?P<question>\d+)/new/$', OptionCreateView.as_view(), name='option_new'),
+    url(r'^vote/$', vote, name="vote"),
 )
--- sujets/views.py
+++ sujets/views.py
@@ -1,3 +1,6 @@
+from django.forms.models import ModelForm
+from django.forms.widgets import HiddenInput
+from django.views.generic.edit import CreateView
 from sujets.models import Question, Option
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
@@ -19,3 +22,30 @@ def vote(request):
 			option.save()
 		return redirect(option.question)
 	return redirect('/')
+
+
+class OptionCreateView(CreateView):
+    model = Option
+
+#    def __init__(self, **kwargs):
+#        super(OptionCreateView, self).__init__(**kwargs)
+#        self.question = get_object_or_404(Question, pk=self.kwargs['question'])
+
+    class form_class(ModelForm):
+        class Meta:
+            model = Option
+            exclude = ('voters')
+            widgets = {'question': HiddenInput()}
+
+    def get_initial(self):
+        self.question = get_object_or_404(Question, pk=self.kwargs['question'])
+        return {'question': self.question}
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+    def get_context_data(self, **kwargs):
+        context = super(OptionCreateView, self).get_context_data(**kwargs)
+        context['question'] = self.question
+        return context
+
