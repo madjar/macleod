f98e3b9bf7033628aefcb07052fc9654270cd444
Georges Dubus <georges.dubus@compiletoi.net>
2011-11-30 16:58:28 +0100

    Little improvments to the admin.

--- sujets/admin.py
+++ sujets/admin.py
@@ -1,5 +1,17 @@
 from sujets.models import *
 from django.contrib import admin
 
-admin.site.register(Question)
-admin.site.register(Option)
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question')
+    list_filter = ('question', )
+
+admin.site.register(Option, OptionAdmin)
